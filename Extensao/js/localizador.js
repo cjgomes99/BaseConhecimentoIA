1c10291bb6dfc67ad17595912b7870d6d878bb09
cjgomes99 <cjgomes@tjes.jus.br>
2026-07-28 18:03:21 +0000

    Ajuste 4.1

--- Extensao/js/localizador.js
+++ Extensao/js/localizador.js
@@ -4,6 +4,13 @@ const Localizador = {
 
         const pesquisa = pergunta.toLowerCase();
 
+        console.log(indice);
+        console.log(pesquisa);
+
+        for (const doc of indice) {
+        console.log(doc.palavrasChave);
+}
+
         return indice.find(doc =>
 
             doc.palavrasChave.some(palavra =>
