7ac557f53302496793cac3a85bde07476a2709ef
cjgomes99 <cjgomes@tjes.jus.br>
2026-07-28 18:15:38 +0000

    4.1

--- Extensao/js/localizador.js
+++ Extensao/js/localizador.js
@@ -4,14 +4,7 @@ const Localizador = {
 
         const pesquisa = pergunta.toLowerCase();
 
-        console.log(indice);
-        console.log(pesquisa);
-
-        for (const doc of indice) {
-        console.log(doc.palavrasChave);
-}
-
-        return indice.find(doc =>
+                return indice.find(doc =>
 
             doc.palavrasChave.some(palavra =>
                 pesquisa.includes(palavra.toLowerCase())
