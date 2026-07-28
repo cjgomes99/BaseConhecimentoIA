ced167b87205db940cfd9665dfc9c7ddaa5c3e15
cjgomes99 <cjgomes@tjes.jus.br>
2026-07-28 18:54:19 +0000

    4.1

--- Extensao/js/api.js
+++ Extensao/js/api.js
@@ -1,37 +1,3 @@
-function converterDocumento(texto) {
-
-    const documento = {};
-
-    const linhas = texto.split("\n");
-
-    let campoAtual = null;
-
-    for (const linha of linhas) {
-
-        const valor = linha.trim();
-
-        if (valor.endsWith(":")) {
-
-            campoAtual = valor.slice(0, -1);
-
-            documento[campoAtual] = "";
-
-            continue;
-
-        }
-
-        if (campoAtual && valor !== "") {
-
-            documento[campoAtual] +=
-                (documento[campoAtual] ? "\n" : "") + valor;
-
-        }
-
-    }
-
-    return documento;
-
-}
 
 function extrairCampo(texto, campo) {
 
