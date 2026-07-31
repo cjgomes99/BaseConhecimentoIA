fc0660fecf43e754df4e576a6ab5544cd3ff2523
cjgomes99 <cjgomes@tjes.jus.br>
2026-07-31 13:48:02 +0000

    TesteIntegra5.2

--- Extensao/js/storage.js
+++ Extensao/js/storage.js
@@ -1,3 +1,21 @@
 const Storage = {
 
+    async salvar(chave, valor) {
+
+        await chrome.storage.local.set({
+
+            [chave]: valor
+
+        });
+
+    },
+
+    async obter(chave) {
+
+        const dados = await chrome.storage.local.get(chave);
+
+        return dados[chave];
+
+    }
+
 };
